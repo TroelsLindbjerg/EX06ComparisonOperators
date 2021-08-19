37fed5658569f0cb3c79e4810c34b86a13a357ee
troe5906 <troe5906@CV-BB-5768>
2021-08-19 09:45:01 +0200

    Add project files.

---
+++ EX06ComparisonOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX06ComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opg.6
+            //Man kan lave et , ved int hvis man har flere variabler (tal) til datatypen(int).
+            int a = 2, b = 4;
+            // ( == ligmed) (<= / >= størrer/mindre eller ligmed) ( != ikke ligmed)
+            Console.WriteLine(a == b);//false
+            Console.WriteLine(a > b);//false
+            Console.WriteLine(a <= b);//true
+            Console.WriteLine(a - b > 0);//false
+            Console.WriteLine(a + b > 0);//true
+            Console.WriteLine(a > 1 - b);//true
+
+        }
+    }
+}
